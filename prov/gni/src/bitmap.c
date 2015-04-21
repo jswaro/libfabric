392121530d8316a80e2cd84e640b322623f33090
James Swaro <jswaro@cray.com>
2015-04-21 17:17:04 -0500

    finished the ff* functions

--- prov/gni/src/bitmap.c
+++ prov/gni/src/bitmap.c
@@ -7,10 +7,117 @@
 
 #include "bitmap.h"
 
+static inline int __find_first_zero_bit(uint8_t val)
+{
+	int i;
+	uint8_t j;
+
+	for (i = 0, j = 1; i < 8; ++i, j <<= 1) {
+		if (!(val & j))
+			break;
+	}
+
+	return i;
+}
+
+static inline int __find_first_zero_byte(gnix_bitmap_value_t *val)
+{
+	int i, pos, ret;
+	uint8_t *iter = (uint8_t *) val;
+	uint8_t full_value = ~0;
+
+	for (i = 0, pos = 0;
+			i < sizeof(gnix_bitmap_value_t);
+			++i, pos += 8) {
+
+		if (iter[i] != full_value) {
+			pos += __find_first_zero_bit(iter[i]);
+			break;
+		}
+	}
+
+	return pos;
+}
+
+int find_first_zero_bit(gnix_bitmap_t *bitmap)
+{
+	int i, pos;
+	gnix_bitmap_block_t *block;
+	gnix_bitmap_value_t full_value = ~0;
+	gnix_bitmap_value_t value;
+
+	for (i = 0, pos = 0; i < BITMAP_BLOCKS(bitmap->length);
+			++i, pos += GNIX_BITMAP_BUCKET_LENGTH) {
+		block = &bitmap->arr[i];
+		value = __gnix_load_block(bitmap, i);
+		if (value != full_value) {
+			pos += __find_first_zero_byte(&value);
+
+			return pos;
+		}
+	}
+
+	return bitmap->length;
+}
+
+static inline int __find_first_set_bit(gnix_bitmap_value_t val)
+{
+
+	int i;
+	uint8_t j;
+
+	for (i = 0, j = 1; i < 8; ++i, j <<= 1) {
+		if (val & j)
+			break;
+	}
+
+	return i;
+}
+
+
+static inline int __find_first_set_byte(gnix_bitmap_value_t *val)
+{
+	int i, pos, ret;
+	uint8_t *iter = (uint8_t *) val;
+
+	for (i = 0, pos = 0;
+			i < sizeof(gnix_bitmap_value_t);
+			++i, pos += 8) {
+
+		if ((iter[i] != 0)) {
+			pos += __find_first_set_bit(iter[i]);
+			break;
+		}
+	}
+
+	return pos;
+}
+
+int find_first_set_bit(gnx_bitmap_t *bitmap)
+{
+	int i, pos;
+	gnix_bitmap_block_t *block;
+	gnix_bitmap_value_t full_value = ~0;
+	gnix_bitmap_value_t value;
+
+	for (i = 0, pos = 0; i < BITMAP_BLOCKS(bitmap->length);
+			++i, pos += GNIX_BITMAP_BUCKET_LENGTH) {
+		block = &bitmap->arr[i];
+		value = __gnix_load_block(bitmap, i);
+		if (value != 0) {
+			pos += __find_first_set_byte(&value);
+
+			return pos;
+		}
+	}
+
+	return bitmap->length;
+}
+
 void fill_bitmap(gnix_bitmap_t *bitmap, int value)
 {
 	int i;
-	gnix_bitmap_block_t fill_value = (value != 0) ? ~0 : 0;
+	gnix_bitmap_value_t fill_value = (value != 0) ? ~0 : 0;
 
 	GNIX_BITMAP_LOCK_ACQUIRE(bitmap);
 
