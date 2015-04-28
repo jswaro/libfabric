9f280d9f90f9ccf50c46becc809b3fba43978895
James Swaro <jswaro@cray.com>
2015-04-28 16:18:44 -0500

    fixed the test cases. I need to add additional tests for set properties

--- prov/gni/test/hashtable.c
+++ prov/gni/test/hashtable.c
@@ -40,11 +40,20 @@ gnix_test_element_t elements[4] = {
 gnix_test_element_t *simple_element = &elements[0];
 gnix_hashtable_t *test_ht = NULL;
 
+void __gnix_hashtable_test_uninitialized(void)
+{
+	assert(test_ht->ht_state == GNIX_HT_STATE_UNINITIALIZED);
+	assert(test_ht->ht_size == 0);
+	assert(test_ht->ht_tbl == NULL);
+}
+
 void __gnix_hashtable_test_setup_bare(void)
 {
 	assert(test_ht == NULL);
 	test_ht = (gnix_hashtable_t *) calloc(1, sizeof(gnix_hashtable_t));
 	assert(test_ht != NULL);
+
+	__gnix_hashtable_test_uninitialized();
 }
 
 
@@ -53,13 +62,6 @@ void __gnix_hashtable_test_teardown_bare(void)
 	assert(test_ht != NULL);
 	free(test_ht);
 	test_ht = NULL;
-}
-
-void __gnix_hashtable_test_uninitialized(void)
-{
-	assert(test_ht->ht_state == GNIX_HT_STATE_UNINITIALIZED);
-	assert(test_ht->ht_size == 0);
-	assert(test_ht->ht_tbl == NULL);
 }
 
 void __gnix_hashtable_test_initialized(void)
@@ -162,10 +164,8 @@ Test(gnix_hashtable_basic, destroy)
 
 	__gnix_hashtable_destroy();
 }
-/*
-Test(gnix_hashtable, destroy_twice,
-		.init = __gnix_hashtable_test_setup_bare,
-		.fini = __gnix_hashtable_test_teardown_bare)
+
+Test(gnix_hashtable_basic, destroy_twice)
 {
 	int ret;
 
@@ -177,14 +177,14 @@ Test(gnix_hashtable, destroy_twice,
 	assert(ret == -EINVAL);
 	__gnix_hashtable_test_destroyed_clean();
 }
-*/
+
 
 /* from this point on in the testing, init functions will already set up the
  *   hash maps and then destroy them at the end of the tests.
  */
 
-/*
-Test(gnix_hashtable, insert_1)
+
+Test(gnix_hashtable_advanced, insert_1)
 {
 	int ret;
 
@@ -194,7 +194,7 @@ Test(gnix_hashtable, insert_1)
 	assert(atomic_get(&test_ht->ht_elements) == 1);
 }
 
-Test(gnix_hashtable, insert_1_remove_1)
+Test(gnix_hashtable_advanced, insert_1_remove_1)
 {
 	int ret;
 
@@ -211,7 +211,8 @@ Test(gnix_hashtable, insert_1_remove_1)
 	assert(atomic_get(&test_ht->ht_elements) == 0);
 }
 
-Test(gnix_hashtable, insert_1024)
+
+Test(gnix_hashtable_advanced, insert_1024)
 {
 	int ret, i;
 
@@ -235,38 +236,46 @@ Test(gnix_hashtable, insert_1024)
 	assert(atomic_get(&test_ht->ht_elements) == 1024);
 }
 
-Test(gnix_hashtable, insert_1024_remove_1024)
+
+Test(gnix_hashtable_advanced, insert_1024_remove_1024)
 {
 	int ret, i;
 
 	gnix_test_element_t test_elements[1024];
+	gnix_test_element_t *item;
 
 	srand(time(NULL));
 
 	for (i = 0; i < 1024; ++i) {
-		test_elements[i].key = rand();
-		test_elements[i].val = rand() % (1024 * 1024);
-		test_elements[i].magic = __GNIX_MAGIC_VALUE;
+		item = &test_elements[i];
+		item->key = i;
+		item->val = rand() % (1024 * 1024);
+		item->magic = __GNIX_MAGIC_VALUE;
 	}
 
 	for (i = 0; i < 1024; ++i) {
+		item = &test_elements[i];
 		ret = gnix_ht_insert(test_ht,
-				test_elements[i].key, &test_elements[i]);
+				item->key, item);
 		assert(ret == 0);
 		assert(atomic_get(&test_ht->ht_elements) == (i + 1));
 	}
 
-	for (i = 1024; i >= 0; --i) {
+	for (i = 1023; i >= 0; --i) {
+		item = &test_elements[i];
+		assert(i == item->key);
+
 		ret = gnix_ht_remove(test_ht,
-				test_elements[i].key);
+				item->key);
 		assert(ret == 0);
-		assert(atomic_get(&test_ht->ht_elements) == (i - 1));
+		assert(atomic_get(&test_ht->ht_elements) == i);
 	}
 
 	assert(atomic_get(&test_ht->ht_elements) == 0);
 }
 
-Test(gnix_hashtable, insert_1_lookup_pass)
+
+Test(gnix_hashtable_advanced, insert_1_lookup_pass)
 {
 	int ret;
 	gnix_test_element_t *found = NULL;
@@ -280,10 +289,9 @@ Test(gnix_hashtable, insert_1_lookup_pass)
 	found = gnix_ht_lookup(test_ht, simple_element->key);
 	assert(found == simple_element);
 	assert(found->magic == __GNIX_MAGIC_VALUE);
-
 }
 
-Test(gnix_hashtable, insert_1_lookup_fail)
+Test(gnix_hashtable_advanced, insert_1_lookup_fail)
 {
 	int ret;
 	gnix_test_element_t *found = NULL;
@@ -299,23 +307,28 @@ Test(gnix_hashtable, insert_1_lookup_fail)
 	assert(found == NULL);
 }
 
-Test(gnix_hashtable, insert_1024_lookup_all)
+Test(gnix_hashtable_advanced, insert_1024_lookup_all)
 {
 	int ret, i;
 	gnix_test_element_t test_elements[1024];
+	gnix_test_element_t *item;
 	gnix_test_element_t *found = NULL;
 
 	srand(time(NULL));
 
 	for (i = 0; i < 1024; ++i) {
-		test_elements[i].key = rand();
-		test_elements[i].val = rand() % (1024 * 1024);
-		test_elements[i].magic = __GNIX_MAGIC_VALUE;
+		item = &test_elements[i];
+
+		item->key = i;
+		item->val = rand() % (1024 * 1024);
+		item->magic = __GNIX_MAGIC_VALUE;
 	}
 
 	for (i = 0; i < 1024; ++i) {
+		item = &test_elements[i];
+
 		ret = gnix_ht_insert(test_ht,
-				test_elements[i].key, &test_elements[i]);
+				item->key, item);
 		assert(ret == 0);
 		assert(atomic_get(&test_ht->ht_elements) == (i + 1));
 	}
@@ -330,23 +343,28 @@ Test(gnix_hashtable, insert_1024_lookup_all)
 	}
 }
 
-Test(gnix_hashtable, insert_1024_lookup_random)
+Test(gnix_hashtable_advanced, insert_1024_lookup_random)
 {
 	int ret, i;
 	gnix_test_element_t test_elements[1024];
 	gnix_test_element_t *found = NULL, *to_find = NULL;
+	gnix_test_element_t *item;
 
 	srand(time(NULL));
 
 	for (i = 0; i < 1024; ++i) {
-		test_elements[i].key = rand();
-		test_elements[i].val = rand() % (1024 * 1024);
-		test_elements[i].magic = __GNIX_MAGIC_VALUE;
+		item = &test_elements[i];
+
+		item->key = i;
+		item->val = rand() % (1024 * 1024);
+		item->magic = __GNIX_MAGIC_VALUE;
 	}
 
 	for (i = 0; i < 1024; ++i) {
+		item = &test_elements[i];
+
 		ret = gnix_ht_insert(test_ht,
-				test_elements[i].key, &test_elements[i]);
+				item->key, item);
 		assert(ret == 0);
 		assert(atomic_get(&test_ht->ht_elements) == (i + 1));
 	}
@@ -361,6 +379,4 @@ Test(gnix_hashtable, insert_1024_lookup_random)
 		assert(found->magic == __GNIX_MAGIC_VALUE);
 	}
 }
-*/
-
 
